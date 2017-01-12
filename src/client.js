10289a97cd2417ead77163858a3d3f82375b4e62
Daniel Vianna <dvianna@bunnings.com.au>
2017-01-12 08:07:43 +0800

    async in use

--- src/client.js
+++ src/client.js
@@ -1,19 +1,22 @@
 'use strict'
 
 const request = require('request')
+const async = require('async')
 
-request.get(
+const getUsername = callback => request.get(
   'http://localhost:9999/getUserName?id=1234',
   (err, res, body) => {
     const result = JSON.parse(body)
-    const name = result.value
-
-    request.get(
-      'http://localhost:9999/getUserStatus?id=1234',
-      (err, res, body) => {
-        const result = JSON.parse(body)
-        const status = result.value
-
-        console.log('The status of user', name, 'is', status)
-      })
+    callback(err, result.value)
   })
+
+const getUserStatus = callback => request.get(
+  'http://localhost:9999/getUserStatus?id=1234',
+  (err, res, body) => {
+    const result = JSON.parse(body)
+    callback(err, result.value)
+  })
+
+async.parallel([getUsername, getUserStatus], (err, results) => {
+  console.log('The status of user', results[0], 'is', results[1])
+})
